0215b2343630b4e5ebcdf92443df935ef098bbac
unknown <trip.rolandas@gmail.com>
2019-07-18 16:40:55 +0700

    more

--- scripts/script.js
+++ scripts/script.js
@@ -105,7 +105,6 @@ class CreatePopupFromJSON {
             "width": "99%",
             "margin": "0px !important"
         };
-        console.log(this.selectorStyle);
         this.optionClass = "popup-selector-option";
         this.optionStyle = {
             "background": "rgba(0, 0, 0, 0.0)",
